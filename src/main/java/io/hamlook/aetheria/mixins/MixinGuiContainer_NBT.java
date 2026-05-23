0e95059a3892cbc3865380219b04d2e52847e20b
Whispering <ginafro240810@gmail.com>
2026-05-23 17:59:32 +0530

    change: made the nbt copy be prettier and easier to debug

--- src/main/java/io/hamlook/aetheria/mixins/MixinGuiContainer_NBT.java
+++ src/main/java/io/hamlook/aetheria/mixins/MixinGuiContainer_NBT.java
@@ -6,10 +6,14 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -27,8 +31,9 @@ public class MixinGuiContainer_NBT {
                 ItemStack stack = this.theSlot.getStack();
 
                 if (stack.hasTagCompound()) {
-                    String nbtString = stack.getTagCompound().toString();
-                    GuiScreen.setClipboardString(nbtString);
+                    String prettyNbt = justEnoughfakepixel$formatNBT(stack.getTagCompound(), 2);
+
+                    GuiScreen.setClipboardString(prettyNbt);
 
                     Minecraft.getMinecraft().thePlayer.addChatMessage(
                             new ChatComponentText(EnumChatFormatting.GREEN + "Copied NBT to clipboard!")
@@ -40,6 +45,38 @@ public class MixinGuiContainer_NBT {
                 }
             }
         }
+    }
+
+    @Unique
+    private static String justEnoughfakepixel$formatNBT(NBTBase nbt, int indent) {
+        StringBuilder builder = new StringBuilder();
+        String spaces = new String(new char[indent]).replace("\0", "  ");
+
+        if (nbt instanceof NBTTagCompound) {
+            NBTTagCompound compound = (NBTTagCompound) nbt;
+            builder.append("{\n");
+            for (String key : compound.getKeySet()) {
+                NBTBase tag = compound.getTag(key);
+                builder.append(spaces).append("  \"").append(key).append("\": ");
+                builder.append(justEnoughfakepixel$formatNBT(tag, indent + 1)).append(",\n");
+            }
+            builder.append(spaces).append("}");
+        }
+        else if (nbt instanceof NBTTagList) {
+            NBTTagList list = (NBTTagList) nbt;
+            builder.append("[\n");
+            for (int i = 0; i < list.tagCount(); i++) {
+                builder.append(spaces).append("  ");
+                builder.append(justEnoughfakepixel$formatNBT(list.get(i), indent + 1)).append(",\n");
+            }
+            builder.append(spaces).append("]");
+        }
+        else {
+            builder.append(nbt.toString());
+        }
+
+        return builder.toString().replaceAll(",\n" +
+                "\\s*([]}])", "\n$1");
     }
 
 }
